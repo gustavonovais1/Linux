4e67dc75f8820a5ad94ce9990f421a9551701b69
gustavonovais1 <gustavonovais401@gmail.com>
2022-07-28 20:02:37 +0000

    Adcionando arquivos

---
+++ iacl.sh
@@ -0,0 +1,41 @@
+!#/bin/bash
+
+echo"Cridiretorios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuarios..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuarios..."
+
+useradd kamilly -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_ADM
+useradd matheus -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_ADM
+useradd pedro -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_ADM
+
+useradd linda -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_VEN
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_VEN
+useradd lucas -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_VEN
+
+useradd jose -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_SEC
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_SEC
+useradd paulo -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_SEC
+
+echo "Expecificando permisões dos diretorios"
+
+chown root:GRP_ADM /adm 
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "FIM..."
